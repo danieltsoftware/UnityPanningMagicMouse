adb868ef9678501711bb9d5fbce0e3bafc99340d
danieltsoftware <157103291+danieltsoftware@users.noreply.github.com>
2024-06-25 15:22:58 +0200

    Update SceneViewPanner.cs
    Fixed unresponsive panning when scene isn't focused.

--- SceneViewPanner.cs
+++ SceneViewPanner.cs
@@ -1,63 +1,67 @@
+using System.Runtime.InteropServices;
 using UnityEngine;
 using UnityEditor;
 
 [InitializeOnLoad]
 public class SceneViewPanner
 {
-	static SceneViewPanner()
-	{
-		SceneView.duringSceneGui += OnSceneGUI;
-	}
+    static SceneViewPanner()
+    {
+        SceneView.duringSceneGui += OnSceneGUI;
+    }
 
-	static Vector3 lastMousePosition;
-	static bool isPanning = false;
+    static Vector2 lastMousePosition = Vector2.zero;
+    static bool isPanning = false;
+    
+    static void OnSceneGUI(SceneView sceneView)
+    {
+        Event e = Event.current;
 
-	static void OnSceneGUI(SceneView sceneView)
-	{
-		Event e = Event.current;
-
-		if (e.type == EventType.MouseDown && e.button == 2)
-		{
-			isPanning = true;
-			lastMousePosition = e.mousePosition;
-			e.Use();
-		}
-		else if (e.type == EventType.MouseUp && e.button == 2)
-		{
-			isPanning = false;
-			e.Use();
-		}
-		else if (e.type == EventType.MouseDrag && isPanning)
-		{
-			Vector3 delta = e.mousePosition - (Vector2)lastMousePosition;
-			delta.y = -delta.y;
-			PanSceneView(sceneView, delta);
-			lastMousePosition = e.mousePosition;
-			e.Use();
-		}
-	}
-
-	static void PanSceneView(SceneView sceneView, Vector3 delta)
-	{
-		Camera cam = sceneView.camera;
+        if (e.type == EventType.MouseDown && e.button == 2)
+        {
+            isPanning = true;
+            lastMousePosition = e.mousePosition;
+            e.Use();
+        }
+        else if (e.type == EventType.MouseUp && e.button == 2)
+        {
+            isPanning = false;
+            e.Use();
+        }
         
-		// Adjust the panning speed
-		float panSpeed = 0.004f;
+        if (isPanning && Event.current != null)
+        {
+            Vector2 currentMousePosition = Event.current.mousePosition;
+            Vector2 delta = currentMousePosition - lastMousePosition;
+            delta.y = -delta.y;
+            lastMousePosition = currentMousePosition;
 
-		if (cam.orthographic)
-		{
-			// Orthographic mode
-			Vector3 movement = cam.transform.right * -delta.x + cam.transform.up * -delta.y;
-			sceneView.pivot += movement * panSpeed * cam.orthographicSize;
-		}
-		else
-		{
-			// Perspective mode
-			float distance = Vector3.Distance(sceneView.pivot, cam.transform.position);
-			Vector3 movement = cam.transform.right * -delta.x + cam.transform.up * -delta.y;
-			sceneView.pivot += movement * panSpeed * distance;
-		}
+            if (sceneView!= null)
+            {
+                PanSceneView(sceneView, delta);
+            }
+        }
+    }
 
-		sceneView.Repaint();
-	}
+    static void PanSceneView(SceneView sceneView, Vector3 delta)
+    {
+        Camera cam = sceneView.camera;
+        float panSpeed = 0.004f;
+
+        if (cam.orthographic)
+        {
+            // Orthographic mode
+            Vector3 movement = cam.transform.right * -delta.x + cam.transform.up * -delta.y;
+            sceneView.pivot += movement * panSpeed * cam.orthographicSize;
+        }
+        else
+        {
+            // Perspective mode
+            float distance = Vector3.Distance(sceneView.pivot, cam.transform.position);
+            Vector3 movement = cam.transform.right * -delta.x + cam.transform.up * -delta.y;
+            sceneView.pivot += movement * panSpeed * distance;
+        }
+
+        sceneView.Repaint();
+    }
 }
